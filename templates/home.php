d103cd4c2493f12790c3628de725439081a655d7
mwilliams <mwilliams@ADVP-INSTRUCTOR>
2016-12-14 11:52:23 -0400

    changed welcome message

--- templates/home.php
+++ templates/home.php
@@ -1,7 +1,7 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
     <div class="container">
-        <h1>Welcome</h1>
+        <h1>Welcome Guest</h1>
         <p>Knowledge is Power is dedicated to keeping you up-to-date on the Web security and programming information you need to know.</p>
         <p>Our experts are real people with real-world technology experience from around the globe. Microsoft MVPs, IT consultants and many more.</p>
         <p><a class="btn btn-success btn-lg" href="/InClassOOPDemos/about.php" role="button">Learn more &raquo;</a></p>
